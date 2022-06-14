edb6dc1e5e49c3530647158f863df66d36064558
sl0cks <91469723+sl0cks@users.noreply.github.com>
2022-06-14 22:41:54 +0300

    Update calculation.h

--- calculation.h
+++ calculation.h
@@ -7,6 +7,6 @@
 double searchMax(char*** data, size_t lines, int numcolumn); //функция поиска максимума
 double searchMin(char*** data, size_t lines, int numcolumn); //функция поиска минимума
 double searchMed(char*** data, size_t lines, int numcolumn); //функция поиска среднего
-double **serchForGraphic(char*** data, size_t lines, int numcolumn); //функция для запоминания значений для графика
+
 
 #endif // CALCULATION_H

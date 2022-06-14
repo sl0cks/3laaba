765554d4564cd5a541fb5ff71c7409f40dbbeed1
sl0cks <91469723+sl0cks@users.noreply.github.com>
2022-06-14 20:49:33 +0300

    Update memory.h

--- memory.h
+++ memory.h
@@ -6,6 +6,6 @@
 
 void clean2DArray(char **arr, size_t size); //функция очищения данных с **
 void clean3DArray(char ***arr, size_t sizeX, size_t sizeY); //функция очищения данных с ***
-double** memoryMatrix(size_t lines); //функция выделения памяти под матрицу
+double** AllocateMatrix(size_t lines); //функция выделения памяти под матрицу
 
 #endif // MEMORY_H

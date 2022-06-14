371275df2e173839a3ed25a5214c5fa573a27a6e
sl0cks <91469723+sl0cks@users.noreply.github.com>
2022-06-15 01:15:26 +0300

    Update logic.cpp

--- logic.cpp
+++ logic.cpp
@@ -29,35 +29,48 @@ FuncReturningValue* entryPoint(FunctionType func, FunctionArgument* funcA) {
 FuncReturningValue* getDataFromFileDo(const char* filename, const char* regiononename) {
     FuncReturningValue* smt = (FuncReturningValue*)calloc(1, sizeof(FuncReturningValue)); //структура, куда считываются данные
     FILE* file = fopen(filename, "r");
-    if(file != NULL || smt != NULL){
+    if(file != NULL && smt != NULL){
         size_t lines, box; //количество строк и колонок
         char **crudeData = readFromFile(file, &lines); //массив строк
-        char ***data = (char***)malloc(sizeof(char**) * (lines - 1)); //массив строк, поделённых на слова
-        if (data == NULL || crudeData == NULL)
+        if (crudeData == NULL) {
             smt->is_file_error = true;
+            return smt;
+        }
+        char ***data = (char***)malloc(sizeof(char**) * (lines - 1)); //массив строк, поделённых на слова
+        if (data == NULL) {
+            smt->is_file_error = true;
+            clean2DArray(crudeData, lines);
+            free(data);
+            return smt;
+        }
         for(size_t i = 0; i < lines - 1; i++) {
            *(data + i) = strSplit(*(crudeData + (i + 1)), &box, ',');
-           if (*(data + i) == NULL)
+           if (*(data + i) == NULL) {
+               clean3DArray(data, lines, i);
                smt->is_file_error = true;
+           }
            free(*(crudeData + (i + 1)));
         }
         lines--;
         char **table_headers = strSplit(*(crudeData + HEADER), &box, ',');
-        if (table_headers == NULL)
+        if (table_headers == NULL) {
+            clean2DArray(table_headers, box);
+            clean3DArray(data, lines, box);
             smt->is_file_error = true;
+            return smt;
+        }
         free(*(crudeData + HEADER));
         free(crudeData);
-
         smt->lines = lines;
         smt->columns = box;
         smt->table_headers = table_headers;
         smt->data = data;
-
         smt = sortingByRegion(smt, regiononename);
-
         fclose(file);
-    } else
+    } else if (file == NULL) {
         smt->is_file_error = true;
+    } else if (smt == NULL)
+        fclose(file);
 
     return smt;
 }
@@ -81,12 +94,11 @@ FuncReturningValue* calculationDataDo(char*** data, size_t lines, int numcolumn)
 void clean_All(FunctionArgument *arg) {
     if(arg->filename != NULL) {
         free(arg->filename);
-    } if(arg->regionname != NULL) {
+    } else if(arg->regionname != NULL) {
         free(arg->regionname);
-    } if(arg->data != NULL) {
+    } else if(arg->data != NULL) {
         clean3DArray(arg->data, arg->lines, arg->columns);
-    } if(arg->table_headers != NULL) {
+    } else if(arg->table_headers != NULL) {
         clean2DArray(arg->table_headers, arg->columns);
     }
 }
-
